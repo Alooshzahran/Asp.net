3441504571dd5e2b3e70a522b50214e5ff261206
Alaa zahran <115067579+Alooshzahran@users.noreply.github.com>
2023-03-01 10:29:28 +0300

    Add files via upload

---
+++ Tasks/task2/task2/WebForm1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_28_11.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <%--<script src="Scripts/sweetalert-dev.js"></script>--%>
+    <script src="Scripts/sweetalert.min.js"></script>
+    <link href="Styles/sweetalert.css" rel="stylesheet"/>
+    <title></title>
+
+      <script type="text/javascript">
+        function successalert() {
+            swal({
+                title: 'Congratulations!',
+                text: 'Your message has been succesfully sent',
+                type: 'success'
+            });
+        }
+      </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+  <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                    </ContentTemplate>
+                </asp:UpdatePanel>
+
+
+          
+
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tasks/task2/task2/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _28_11
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //SqlConnection cc = new SqlConnection("data source=DESKTOP-V50HPE1\\SQLEXPRESS; database=comment_Task; integrated security=SSPI");
+            //cc.Open();
+
+
+           
+            //cc.Close();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            ScriptManager.RegisterStartupScript(this, GetType(), "Popup", "successalert();", true);
+        
+
+        SqlConnection cc = new SqlConnection("data source=DESKTOP-V50HPE1\\SQLEXPRESS; database=comment_Task; integrated security=SSPI");
+            cc.Open();
+
+
+            string query = "insert into comment (comment) values ('" + TextBox1.Text + "') ";
+            SqlCommand cmd = new SqlCommand(query, cc);
+            cmd.ExecuteNonQuery();
+            ////////////////////////////////////////////////////////////////////////////
+            ///
+
+
+            string query1 = "select * from comment ";
+            SqlCommand cmd1 = new SqlCommand(query1, cc);
+
+            SqlDataReader sdr = cmd1.ExecuteReader();
+            Label1.Text = "";
+            while (sdr.Read())
+            {
+
+                Label1.Text += sdr[1];
+                Label1.Text += "<br/>";
+
+            }
+            cc.Close();
+
+            
+
+
+
+            //Label1.Text =
+        }
+    }
+}
